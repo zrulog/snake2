b1c930de8b2d6c1aaec0ac04cb6f44ad2517aba6
zrulog <kotulev@zruchno.biz>
2016-03-29 18:46:14 +0300

    Процедурный подход. Функция.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,7 +10,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            Console.SetCursorPosition(x2, y2);
+            Console.Write(sym2);
             Console.ReadLine();
         }
     }
